f7d0d77c4121fd7066e6a4e01100fe55ee9b6719
Chris <97125924+ringufinancial@users.noreply.github.com>
2022-01-18 22:01:59 -0800

    Fixed FundedMasterCharge

--- Solidity/pools/FundedMasterCharge/contracts/ContractWhitelisted.sol
+++ Solidity/pools/FundedMasterCharge/contracts/ContractWhitelisted.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-import '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
-import '@openzeppelin/contracts/access/AccessControlEnumerable.sol';
-import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+import './@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
+import './@openzeppelin/contracts/access/AccessControlEnumerable.sol';
+import './@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 
 abstract contract ContractWhitelisted is AccessControlEnumerable {
 	using EnumerableSet for EnumerableSet.AddressSet;
